28737bbf71147ea1d5b1546987d982da0156b210
sven700c <xsvenx@gmail.com>
2014-11-23 15:38:17 +0800

    added dict from pytz

--- update_timezones.py
+++ update_timezones.py
@@ -2,7 +2,7 @@
 # forked from Aleksandr Pasechnik
 # updates by Sven Simon
 #
-# Goes through the Day One jounal and sets the Time Zone of each entry that
+# Goes through the Day One journal and sets the Time Zone of each entry that
 # doesn't correspond with the location to the value of the *timezone* variable. 
 # Makes a backup copy of each entry it modified by adding a '.tzbak' to the filename.
 # Ignores any entry that already has a '.tzbak' version.
@@ -17,6 +17,7 @@ import argparse
 import plistlib
 from shutil import move
 import glob
+from pytz import country_names, country_timezones
 
 
 def write_file(filename, entry):
@@ -31,14 +32,17 @@ def write_file(filename, entry):
     print ''
 
 
-def check_timezone(entry, location):
-    tz_dict = {
-        'Japan': 'Asia/Tokyo',
-        'Singapore': 'Asia/Singapore',
-        'Switzerland': 'Europe/Zurich'
-    }
+def get_tzdict():
+    tz_dict = {}
+    for country_code in country_names:
+        if country_timezones.has_key(country_code):
+            tz_dict[country_names[country_code]] = country_timezones[country_code]
+    return tz_dict
 
-    timezone = tz_dict[location]
+
+def check_timezone(entry, location):
+    tz_dict = get_tzdict()
+    timezone = tz_dict[location][0]
     tz = entry['Time Zone']
     country = entry['Location']['Country']
     if country == location and tz != timezone:
@@ -47,14 +51,16 @@ def check_timezone(entry, location):
         entry['Starred'] = 'true'
         return entry
 
+
 def default_dir():
     config_path = '~/Library/Group Containers/*.dayoneapp/data/Preferences/dayone.plist'
     dayone_conf = plistlib.readPlist(glob.glob(path.expanduser(config_path))[0])
     return str(dayone_conf['JournalPackageURL'] + '/entries')
 
+
 def main(argv):
     args = argparse.ArgumentParser()
-    args.add_argument('-n', '--nowrite', action='store_true', help='dry run')
+    args.add_argument('-w', '--write', action='store_true', help='write changes to files')
     args.add_argument('-p', '--path', help='override default journal entries path')
     args.add_argument('-l', '--location', help='country for which to check timezone entries', default='Japan')
     args.add_argument('-e', '--entry', help='print out single entry')
@@ -81,10 +87,11 @@ def main(argv):
             filename = path.join(base_dir, file)
             entry = plistlib.readPlist(filename)
             update = check_timezone(entry, flag.location)
-            if update and not flag.nowrite:
+            if update and flag.write:
                 write_file(filename, update)
 
     print 'Done.'
+
 
 if __name__ == "__main__":
     main(argv)
